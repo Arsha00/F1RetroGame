8ffe826e3512ba17805c2bcab2f032f685107341
Arsha00 <79259244+Arsha00@users.noreply.github.com>
2025-10-02 11:02:44 +0200

    Update Track.cs
    Larger Track length, finish line added.

--- Models/Track.cs
+++ Models/Track.cs
@@ -21,40 +21,53 @@ namespace Formula1Retro
         private readonly int width;
         private readonly int height;
 
-        public Track(int width = 800, int height = 600) // default storlek
+        public int FinishLineY { get; }
+
+        public Track(int width = 800, int height = 3000) // längre bana
         {
             this.width = width;
             this.height = height;
+
+            // Placera mållinjen högt upp
+            FinishLineY = 50;
         }
 
         public void Draw(DrawingContext dc)
         {
             // Gräs på båda sidor
-            dc.DrawRectangle(Brushes.Green, null, new System.Windows.Rect(0, 0, width, height));
+            dc.DrawRectangle(Brushes.Green, null, new Rect(0, 0, width, height));
 
             // Vägen (400px bred i mitten)
             int roadWidth = 400;
             int roadX = (width - roadWidth) / 2;
-            dc.DrawRectangle(Brushes.DarkSlateGray, null, new System.Windows.Rect(roadX, 0, roadWidth, height));
+            dc.DrawRectangle(Brushes.DarkSlateGray, null, new Rect(roadX, 0, roadWidth, height));
 
             // Kantlinjer (vita linjer på sidorna)
-            dc.DrawRectangle(Brushes.White, null, new System.Windows.Rect(roadX, 0, 4, height));
-            dc.DrawRectangle(Brushes.White, null, new System.Windows.Rect(roadX + roadWidth - 4, 0, 4, height));
+            dc.DrawRectangle(Brushes.White, null, new Rect(roadX, 0, 4, height));
+            dc.DrawRectangle(Brushes.White, null, new Rect(roadX + roadWidth - 4, 0, 4, height));
 
             // Mittlinje (prickad)
             for (int y = 0; y < height; y += 40)
             {
-                dc.DrawRectangle(Brushes.White, null, new System.Windows.Rect(roadX + roadWidth / 2 - 2, y, 4, 20));
+                dc.DrawRectangle(Brushes.White, null, new Rect(roadX + roadWidth / 2 - 2, y, 4, 20));
             }
 
-            // Start- och mållinje (svartvit rutig längst ned)
+            // Startlinje (nere vid botten)
             int lineHeight = 20;
             int squareSize = 20;
             for (int x = 0; x < roadWidth; x += squareSize)
             {
                 Brush brush = (x / squareSize) % 2 == 0 ? Brushes.White : Brushes.Black;
-                dc.DrawRectangle(brush, null, new System.Windows.Rect(roadX + x, height - lineHeight, squareSize, lineHeight));
+                dc.DrawRectangle(brush, null, new Rect(roadX + x, height - lineHeight, squareSize, lineHeight));
+            }
+
+            // Mållinje (uppe vid toppen)
+            for (int x = 0; x < roadWidth; x += squareSize)
+            {
+                Brush brush = (x / squareSize) % 2 == 0 ? Brushes.White : Brushes.Black;
+                dc.DrawRectangle(brush, null, new Rect(roadX + x, FinishLineY, squareSize, lineHeight));
             }
         }
     }
 }
+
